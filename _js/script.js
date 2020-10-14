03ce0f90452664b42bbadf6d40ea663c03f5df41
Nicholas Araújo <njaf@ic.ufal.br>
2020-10-14 18:43:38 -0300

    Added loop on arrows

--- _js/script.js
+++ _js/script.js
@@ -1,10 +1,9 @@
 var indiceControl = 0;
-var btnElementNext = document.getElementsByClassName('btn-next');
-var btnElementPrevious = document.getElementsByClassName('btn-previous');
-var itemsText = document.getElementsByClassName('item-text');
+var btnElementNext = document.getElementsByClassName("btn-next");
+var btnElementPrevious = document.getElementsByClassName("btn-previous");
+var itemsText = document.getElementsByClassName("item-text");
 
-itemsText[0].style.display = 'block';
-
+itemsText[0].style.display = "block";
 
 btnElementNext[0].addEventListener("click", next);
 btnElementPrevious[0].addEventListener("click", previous);
@@ -13,6 +12,10 @@ function next() {
   if (indiceControl < itemsText.length - 1) {
     hideAll();
     indiceControl++;
+    itemsText[indiceControl].style.display = "block";
+  } else {
+    hideAll();
+    indiceControl = 0;
     itemsText[indiceControl].style.display = "block";
   }
 }
@@ -25,8 +28,7 @@ function previous() {
 }
 
 function hideAll() {
-   for (let i =0; i < itemsText.length; i++) {
-     itemsText[i].style.display = 'none';
-   }
+  for (let i = 0; i < itemsText.length; i++) {
+    itemsText[i].style.display = "none";
+  }
 }
-
